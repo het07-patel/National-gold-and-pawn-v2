c87abb549f794830d3cb4ae781585f2780b2ce2b
smruti <smrutidoris@gmail.com>
2025-06-09 11:38:23 +0530

    change in ui page of electronic

--- src/app/what-we-buy/electronics-and-tools/page.jsx
+++ src/app/what-we-buy/electronics-and-tools/page.jsx
@@ -61,16 +61,16 @@ export default function LaptopSection() {
         imgAltAttr="National Gold and Pawn | Diamonds, Watches, Jewelry"
       />
 
-      <div className="text-3xl mb-4 sm:text-4xl md:text-5xl lg:text-6xl text-center pt-8 sm:pt-12 md:pt-16 lg:pt-14 xl:pt-18">
+      <div className="text-3xl mb-4 sm:text-4xl md:text-5xl lg:text-6xl text-center pt-10 md:pt-14 xl:pt-16 2xl:pt-24">
         <h3 className="my-6 md:mb-9 lg:mt-0 text-center text-2xl md:text-3xl lg:text-5xl 2xl:text-6xl font-black uppercase">
           Smart
           <span className="bg-gradient-to-r from-[#eb2a1d] to-[#1d46eb] bg-clip-text text-transparent"> Electronic</span>
         </h3>
       </div>
 
-      <main className="text-xl min-h-screen py-4 px-4 sm:px-6 md:px-8 lg:px-12 xl:px-18 text-center ">
+      <main className="text-xl min-h-screen py-4 px-4 sm:px-6 md:px-8 lg:px-12 xl:px-18 text-center">
         <section className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-12">
-          <div className="relative aspect-[7/7] sm:aspect-[5/5] md:aspect-[7/7] lg:aspect-[7/5] w-full">
+          <div className="relative aspect-[7/7] sm:aspect-[5/5] md:aspect-[6/7] lg:aspect-[7/5] w-full">
             <Image
               src={Image1}
               alt="Model with green bag"
@@ -81,7 +81,7 @@ export default function LaptopSection() {
 
           <div className="flex flex-col justify-center">
             <div className="flex items-center justify-center h-full">
-              <div className="text-base sm:text-lg leading-relaxed max-w-md sm:text-center lg:text-start text-center">
+              <div className="text-base sm:text-lg leading-relaxed max-w-md sm:text-center lg:text-start text-center md:text-left">
                 <h2 className="uppercase text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl 2xl:text-4xl mb-2 sm:mb-3 md:mb-4 leading-tight font-belleza lg:leading-[50px] 2xl:leading-[60px] font-bold ">
                   Experience Seamless Digital Integration
                 </h2>
